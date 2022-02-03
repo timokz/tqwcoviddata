e5e1063be31280dc357f7214013598ae9068ee7f
Timo Keber <tkeber@tqw.at>
2022-02-03 17:49:37 +0100

    pw reset

---
+++ lib/pw_reset_dialog.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+@override
+Widget build(BuildContext context) => TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Show Dialog'),
+    );
